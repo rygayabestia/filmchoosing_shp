b1d180bfe8b0e566f264ae13de9e17bd51aeb7b1
bestaya <wayurits@gmail>
2025-04-24 12:25:10 +0300

    миниплеер

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 AUTH_USER_MODEL = 'users.User'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
